eb945c12bf52483ed580327e5925aef726747888
CODept <you@example.com>
2017-07-24 09:57:49 +0530

    first file

---
+++ css/src/Md5.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;	
+import java.math.*;
+import java.security.*;
+
+public class Md5 {
+
+	public static String getMD5(String input){
+		try{
+			MessageDigest md=MessageDigest.getInstance(("md5");
+		}
+		
+	}
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String plaintext;
+		Scanner sc=new Scanner(System.in);	
+		
+		
+	
+	}
+
+}
---
+++ css/src/cipher.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+public class cipher
+{
+public static void main(String[] args) 
+{
+    Scanner sc=new Scanner(System.in);
+    String str;
+    String key;
+    int keyLength;
+
+    System.out.println("Enter message:");
+    str=sc.nextLine();
+    System.out.println("Enter encryption key:");
+    key=sc.next();
+    keyLength=key.length();
+    
+    System.out.println("Encrypted message..."+encrypt(str,keyLength));
+    System.out.println("Decryptedmessage..."+decrypt(encrypt(str,keyLength),keyLength));
+
+
+    
+   
+}
+public static String encrypt(String str,int keyLength)
+{
+    String cipher="";
+    for(int i=0;i<str.length();i++)
+    {
+        int c=str.charAt(i);
+        if(Character.isUpperCase(c))
+        {
+            c=c+(keyLength%26); 
+            if(c>'Z')
+                c=c-26;
+        }
+        else if(Character.isLowerCase(c))
+        {
+            c=c+(keyLength%26);
+            if(c>'z')
+                c=c-26;
+        }
+        System.out.println(c);
+        cipher=cipher+(char) c;
+        
+    }
+    return cipher;
+}
+public static String decrypt(String str,int keyLength)
+{
+    String decrypted="";
+    for(int i=0;i<str.length();i++)
+    {
+        int c=str.charAt(i);
+        if(Character.isUpperCase(c))
+        {
+            c=c-(keyLength%26);
+            if(c<'A')
+                c=c+26;
+        }
+        else if(Character.isLowerCase(c))
+        {
+            c=c-(keyLength%26);
+            if(c<'a')
+                c=c+26;
+        }
+        System.out.println(c);
+        decrypted=decrypted+(char) c;
+    }
+    return decrypted;
+}
+}
